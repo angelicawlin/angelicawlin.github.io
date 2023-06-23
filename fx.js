5d48a8c9febd156b7a9abfd5b88fab61368151b0
angelicawlin <116993617+angelicawlin@users.noreply.github.com>
2023-06-23 15:04:05 -0400

    made lightbulb adjust color according to lighting

--- fx.js
+++ fx.js
@@ -3,3 +3,14 @@ function toDarkMode()
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+// function toLightMode()
+// {
+//     element.classList.toggle("dark-mode");
+// }
+
+// function switchBulb()
+// {
+//     document.getElementById("lightbulb").src="img/lightoff.png";
+//     document.getElementById("lightswitch").onclick(toLightMode());
+// }
