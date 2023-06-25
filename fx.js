49f2aa7436fb4a0420337f01098d4237a3bb28dd
angelicawlin <116993617+angelicawlin@users.noreply.github.com>
2023-06-25 02:29:09 -0400

    cleaned up code and worked on js

--- fx.js
+++ fx.js
@@ -4,6 +4,25 @@ function toDarkMode()
     element.classList.toggle("dark-mode");
 }
 
+
+let observerOptions = {
+
+    threshold: 0.25,
+};
+
+let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add("item-appear");
+            return;
+        }
+        square.classList.remove('item-appear'); //make item disappear when out of view
+    })
+}, observerOptions)
+
+observer.observe(document.querySelector('.item-wrapper'));
+
 // function toLightMode()
 // {
 //     element.classList.toggle("dark-mode");
